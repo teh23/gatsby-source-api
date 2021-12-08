6e99435ae2cc53007534fb27fb5df4556e8d3b5e
Andrzej Franek <tehix1@gmail.com>
2021-12-08 16:48:12 +0100

    refactor: clean reporter

--- utils/normalizeKeys.js
+++ utils/normalizeKeys.js
@@ -1,20 +1,15 @@
-const _ = require("lodash")
-
-function checkKey(key, reporter) {
+function checkKey(key) {
   const restrictedKeys = ["id", "children", "parent", "fields", "internal"]
   if (restrictedKeys.includes(key)) {
-    // reporter.warn(
-    //   `The key "${key}" is restricted in GraphQL! Transformed to ${key}__normalized.`
-    // )
     return `${key}__normalized`
   }
   return key
 }
 
-module.exports = (apiData, reporter) => {
+module.exports = apiData => {
   const dataNormalized = {}
   Object.keys(apiData).forEach(key => {
-    dataNormalized[checkKey(key, reporter)] = apiData[key]
+    dataNormalized[checkKey(key)] = apiData[key]
   })
   return dataNormalized
 }
